e1cd7864d8cf142468e9a504601ff11b75cf6542
Emilien942702 <milou.barbidur@yahoo.fr>
2018-03-17 22:13:21 +0100

    Added weleakinfo.com
    Added weleakinfo.com + name search + fixed timers

--- SimpleScanner.sh
+++ SimpleScanner.sh
@@ -3,6 +3,7 @@ nbrlignes=$(grep -c ^ $database)
 echo Which database should I use ?
 echo 1. haveibeenpwned.com
 echo 2. hacked-emails.com
+echo 3. weleakinfo.com
 read option
 if [[ $option == "1" ]]; then
 	for ligne in `seq 1 $nbrlignes`;
@@ -18,13 +19,12 @@ if [[ $option == "1" ]]; then
 		if [[ `echo $resultpaste | grep '\[{' -c` -ne 0 ]]; then
 			echo $resultpaste | jq 'map(if .Source=="Pastebin" then .Id="http://pastebin.com/"+.Id else . end)' | jq 'del (.[] | .Source, .Date, .EmailCount)' | jq '{Pastes:[.[]]}' > temppaste
 		fi
-		sleep 2
 		urlbreachedaccount=https://haveibeenpwned.com/api/v2/breachedaccount/$vraieligne
 		urlbreachedaccount="$(echo -e "${urlbreachedaccount}" | tr -d '[:space:]')"
 		resultbreachedaccount=`curl -s -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlbreachedaccount`
 		if [[ `echo $resultbreachedaccount | grep 'Rate limit exceeded' -c` -ne 0 ]]; then
 			echo Rate limit exceeded
-			sleep 2
+			sleep 1.6
 			resultbreachedaccount=`curl -s -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlbreachedaccount`
 			if [[ `echo $resultbreachedaccount | grep 'Rate limit exceeded' -c` -ne 0 ]]; then
 				echo You just got timed out ... Try later 
@@ -57,10 +57,9 @@ if [[ $option == "1" ]]; then
 			fi
 		rm tempsite temppaste newresult 2> /dev/null
 		echo $ligne out of $nbrlignes
-		sleep 2
+		sleep 1.6
 	done
 elif [[ $option == "2" ]]; then
-	url='https://hacked-emails.com/api?q='
 	for ligne in `seq 1 $nbrlignes`;
 	do
 		touch newresult
@@ -93,8 +92,116 @@ elif [[ $option == "2" ]]; then
 		fi
 		rm newresult		
 		echo $ligne out of $nbrlignes
-		sleep 2
 	done
+elif [[ $option == "3" ]]; then
+	echo "Want to try the name search ? [y/n]"
+	read choice
+	if [[ $choice == "y" ]]; then
+		echo "What are your emails made of ?"
+		echo "1. firstname.lastname@domain.com"
+		echo "2. lastname.firstname@domain.com"
+		read choice2
+		for ligne in `seq 1 $nbrlignes`;
+		do
+			touch tempsite
+			touch tempname
+			touch newresult
+			vraieligne=`head -n $ligne $database | tail -1`
+			vraieligne=${vraieligne%$'\r'}
+			if [[ $choice2 == "1" ]]; then
+				prenom=`echo $vraieligne | cut -d '.' -f 1`
+				nom=`echo $vraieligne | cut -d '.' -f 2 | cut -d '@' -f 1`
+			elif [[ $choice2 == "2" ]]; then
+				prenom=`echo $vraieligne | cut -d '.' -f 2 | cut -d '@' -f 1`
+				nom=`echo $vraieligne | cut -d '.' -f 1`
+			fi
+			urlsite='https://api.weleakinfo.com/v2/public/email/'$vraieligne
+			urlsite="$(echo -e "${urlsite}" | tr -d '[:space:]')"
+			resultsite=`curl -s --user-agent "Mozilla 5.0" -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlsite`
+			if [[ `echo $resultsite | grep -w "\"Hits\": 0," -c` -eq 0 ]]; then
+				echo $resultsite | jq 'del(.Response, .Time, .Hits,.Unique)' | jq '{"Sites by email":[.[]]}' > tempsite
+			fi
+			urlname='https://api.weleakinfo.com/v2/public/name/'$prenom\_$nom
+			urlname="$(echo -e "${urlname}" | tr -d '[:space:]')"
+			resultname=`curl -s --user-agent "Mozilla 5.0" -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlname`
+			if [[ `echo $resultname | grep -w "\"Hits\": 0," -c` -eq 0 && `echo $resultname | grep -w "Bad Request" -c` -eq 0 ]]; then
+				echo $resultname | jq 'del(.Response, .Time, .Hits,.Unique)' | jq '{"Sites by name":[.[]]}' > tempname
+			fi
+			if [[ `echo $resultname | grep 'limited' -c` -ne 0 ]]; then
+				echo Rate limit exceeded
+				echo 190 seconds cooldown
+				sleep 190
+				resultname=`curl -s --user-agent "Mozilla 5.0" -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlname`
+				if [[ `echo $resultname | grep 'limited' -c` -ne 0 ]]; then
+					echo You just got timed out ... Try later 
+					last=`expr $ligne - 1`
+					echo "Last mail tested was" 
+					head -n $last $database | tail -1
+					cat result.json | jq '{Emails:[.]}' | sponge result.json
+					sed -i 's/\\t//g' result.json
+					rm newresult
+					exit
+				fi
+			fi
+			if [[ -s tempname && -s tempsite ]]; then
+				jq -s '[.[]]' tempsite tempname | jq "{\"$vraieligne\":[.]}" > newresult
+			elif [[ -s tempname && ! -s tempsite ]]; then
+				cat tempname | jq "{\"$vraieligne\":[.]}" > newresult
+			elif [[ ! -s tempname && -s tempsite ]]; then
+				cat tempsite | jq "{\"$vraieligne\":[.]}" > newresult
+			fi
+			if [[ -s newresult ]]; then
+				echo Found entries for $vraieligne
+				if [[ ! -s result.json ]]; then
+					cat newresult > result.json
+				else
+					 jq -s 'add' result.json newresult | sponge result.json
+				fi
+			fi
+			rm tempsite tempname newresult 2> /dev/null
+			echo $ligne out of $nbrlignes
+	done
+	elif [[ $choice == "n" ]]; then
+		for ligne in `seq 1 $nbrlignes`;
+		do
+			touch newresult
+			vraieligne=`head -n $ligne $database | tail -1`
+			vraieligne=${vraieligne%$'\r'}
+			url='https://api.weleakinfo.com/v2/public/email/'$vraieligne
+			url="$(echo -e "${url}" | tr -d '[:space:]')"
+			result=`curl -s --user-agent "Mozilla 5.0" -H "Accept: application/json" -H "Content-Type: application/json" -X GET $url`
+			if [[ `echo $result | grep 'limited' -c` -ne 0 ]]; then
+				echo Rate limit exceeded
+				echo 190 seconds cooldown
+				sleep 190
+				result=`curl -s --user-agent "Mozilla 5.0" -H "Accept: application/json" -H "Content-Type: application/json" -X GET $url`
+				if [[ `echo $result | grep 'limited' -c` -ne 0 ]]; then
+					echo You just got timed out ... Try later 
+					last=`expr $ligne - 1`
+					echo "Last mail tested was" 
+					head -n $last $database | tail -1
+					cat result.json | jq '{Emails:[.]}' | sponge result.json
+					sed -i 's/\\t//g' result.json
+					rm newresult
+					exit
+				fi
+			fi
+			if [[ `echo $result | grep -w "\"Hits\": 0," -c` -eq 0 ]]; then
+				echo $result | jq 'del(.Response, .Time, .Hits,.Unique)' | jq '{Results:[.[]]}' > newresult
+			fi
+			if [[ -s newresult ]]; then
+				cat newresult | jq "{\"$vraieligne\":[.]}" | sponge newresult
+				echo Found entries for $vraieligne
+				if [[ ! -s result.json ]]; then
+					cat newresult > result.json
+				else
+				 jq -s 'add' result.json newresult | sponge result.json
+				fi
+			fi
+			rm newresult		
+			echo $ligne out of $nbrlignes
+		done
+	fi
 fi
 cat result.json | jq '{Emails:[.]}' | sponge result.json
 sed -i 's/\\t//g' result.json
