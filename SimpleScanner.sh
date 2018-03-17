a1ca0295255114cace449c1e2292ae611e7b0a29
Emilien942702 <milou.barbidur@yahoo.fr>
2018-03-17 15:32:49 +0100

    Bug correction
    Corrected destructive overwriting, API limit and simplified code

--- SimpleScanner.sh
+++ SimpleScanner.sh
@@ -15,19 +15,6 @@ if [[ $option == "1" ]]; then
 		urlpaste=https://haveibeenpwned.com/api/v2/pasteaccount/$vraieligne'?includeUnverified=true'
 		urlpaste="$(echo -e "${urlpaste}" | tr -d '[:space:]')"
 		resultpaste=`curl -s -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlpaste`
-		if [[ `echo $resultpaste | grep 'Rate limit exceeded' -c` -ne 0 ]]; then
-			echo Rate limit exceeded
-			sleep 2
-			resultpaste=`curl -s -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlpaste`
-			if [[ `echo $resultpaste | grep 'Rate limit exceeded' -c` -ne 0 ]]; then
-				echo You just got timed out ... Try later 
-				echo "Last mail tested was" head -n $ligne-1
-				cat result.json | jq '{Emails:[.]}' > result.json
-				sed -i 's/\\t//g' result.json
-				rm tempsite temppaste newresult
-				exit
-			fi
-		fi
 		if [[ `echo $resultpaste | grep '\[{' -c` -ne 0 ]]; then
 			echo $resultpaste | jq 'map(if .Source=="Pastebin" then .Id="http://pastebin.com/"+.Id else . end)' | jq 'del (.[] | .Source, .Date, .EmailCount)' | jq '{Pastes:[.[]]}' > temppaste
 		fi
@@ -35,30 +22,39 @@ if [[ $option == "1" ]]; then
 		urlbreachedaccount=https://haveibeenpwned.com/api/v2/breachedaccount/$vraieligne
 		urlbreachedaccount="$(echo -e "${urlbreachedaccount}" | tr -d '[:space:]')"
 		resultbreachedaccount=`curl -s -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlbreachedaccount`
+		if [[ `echo $resultbreachedaccount | grep 'Rate limit exceeded' -c` -ne 0 ]]; then
+			echo Rate limit exceeded
+			sleep 2
+			resultbreachedaccount=`curl -s -H "Accept: application/json" -H "Content-Type: application/json" -X GET $urlbreachedaccount`
+			if [[ `echo $resultbreachedaccount | grep 'Rate limit exceeded' -c` -ne 0 ]]; then
+				echo You just got timed out ... Try later 
+				last=`expr $ligne - 1`
+				echo "Last mail tested was" 
+				head -n $last $database | tail -1
+				cat result.json | jq '{Emails:[.]}' | sponge result.json
+				sed -i 's/\\t//g' result.json
+				rm temppaste tempsite newresult
+				exit
+			fi
+		fi
 		if [[ `echo $resultbreachedaccount | grep '\[{' -c` -ne 0 ]]; then
 			echo $resultbreachedaccount | jq 'del (.[] | .Title, .Domain, .BreachDate, .AddedDate, .ModifiedDate, .PwnCount, .Description, .IsVerified, .IsFabricated, .IsSensitive, .IsActive, .IsRetired, .IsSpamList, .LogoType)'| jq '{Sites:[.[]]}' > tempsite
 		fi
 		if [[ -s temppaste && -s tempsite ]]; then
 			jq -s '[.[]]' tempsite temppaste | jq "{\"$vraieligne\":[.]}" > newresult
-			rm tempsite
-			rm temppaste
 		elif [[ -s temppaste && ! -s tempsite ]]; then
 			cat temppaste | jq "{\"$vraieligne\":[.]}" > newresult
-			rm temppaste
 		elif [[ ! -s temppaste && -s tempsite ]]; then
 			cat tempsite | jq "{\"$vraieligne\":[.]}" > newresult
-			rm tempsite
 		fi
 			if [[ -s newresult ]]; then
 				echo Found entries for $vraieligne
 				if [[ ! -s result.json ]]; then
 					cat newresult > result.json
 				else
-				 jq -s 'add' result.json newresult > temp
-				 cat temp > result.json
-				 rm temp
+				 jq -s 'add' result.json newresult | sponge result.json
 				fi
-			rm newresult
+			rm tempsite temppaste newresult 2> /dev/null
 			fi
 		echo $ligne out of $nbrlignes
 		sleep 2
@@ -67,7 +63,7 @@ elif [[ $option == "2" ]]; then
 	url='https://hacked-emails.com/api?q='
 	for ligne in `seq 1 $nbrlignes`;
 	do
-		touch temp
+		touch newresult
 		vraieligne=`head -n $ligne $database | tail -1`
 		vraieligne=${vraieligne%$'\r'}
 		url='https://hacked-emails.com/api?q='$vraieligne
@@ -78,31 +74,27 @@ elif [[ $option == "2" ]]; then
 			last=`expr $ligne - 1`
 			echo "Last mail tested was" 
 			head -n $last $database | tail -1
-			rm temp
-			cat result.json | jq '{Emails:[.]}' > result.json
+			rm newresult
+			cat result.json | jq '{Emails:[.]}' | sponge result.json
 			sed -i 's/\\t//g' result.json
 			exit
 		fi
 		if [[ `echo $result | grep -w "found" -c` -ne 0 ]]; then
-			echo $result | jq 'del(.| .status,.results,.query,.data[].author,.data[].verified,.data[].date_created,.data[].date_leaked,.data[].emails_count,.data[].source_lines,.data[].source_size,.data[].source_network,.data[].source_provider)' | jq '{Results:[.[]]}' > temp
-		fi
-		if [[ -s temp ]]; then
-			cat temp | jq "{\"$vraieligne\":[.]}" > newresult
+			echo $result | jq 'del(.| .status,.results,.query,.data[].author,.data[].verified,.data[].date_created,.data[].date_leaked,.data[].emails_count,.data[].source_lines,.data[].source_size,.data[].source_network,.data[].source_provider)' | jq '{Results:[.[]]}' > newresult
 		fi
 		if [[ -s newresult ]]; then
-				echo Found entries for $vraieligne
-				if [[ ! -s result.json ]]; then
-					cat newresult > result.json
-				else
-				 jq -s 'add' result.json newresult > temp
-				 cat temp > result.json
-				 rm temp
-				fi
-			rm newresult
+			cat newresult | jq "{\"$vraieligne\":[.]}" | sponge newresult
+			echo Found entries for $vraieligne
+			if [[ ! -s result.json ]]; then
+				cat newresult > result.json
+			else
+			 jq -s 'add' result.json newresult | sponge result.json
 			fi
+			rm newresult
+		fi		
 		echo $ligne out of $nbrlignes
 		sleep 2
 	done
 fi
-cat result.json | jq '{Emails:[.]}' > result.json
+cat result.json | jq '{Emails:[.]}' | sponge result.json
 sed -i 's/\\t//g' result.json
